2881f268098532bf85310b1f3274f87e1be48f5e
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2024-06-06 11:21:37 -0300

    Aula 06/06

--- 08-Chain Of Responsability/src/index.ts
+++ 08-Chain Of Responsability/src/index.ts
@@ -10,12 +10,12 @@ function setPromptQuestions(){
     const rl = readline.createInterface({
         input: process.stdin, output: process.stdout
     });
-    rl.question("Digite seu Email: ", email =>(
+    rl.question("Digite seu Email: ", email => {
         rl.question("Digite sua Senha: ", password => {
             server.logIn(email, password);
             rl.close();
         });
-    ));
+    });
     rl.on("close", () => setPromptQuestions());
 }
 
